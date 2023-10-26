e378a34c7c12bd3ee4782efaeab94711528794f1
Nallamilli Naveen Sai Reddy <137030267+NaveenSaiReddy99@users.noreply.github.com>
2023-10-26 22:54:51 +0530

    Add files via upload

---
+++ passwordgen.py
@@ -0,0 +1,18 @@
+import string
+import random
+
+len=int(input("Enter length of the password:"))
+lower=string.ascii_lowercase
+upper=string.ascii_uppercase
+digit=string.digits
+symbols=string.punctuation
+
+str=lower+upper+digit+symbols
+str=string.printable
+
+pwd=random.sample(str,len)
+
+password="".join(pwd)
+
+
+print("password is:",password)
